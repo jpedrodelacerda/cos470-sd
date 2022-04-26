6665de15e144093df1df692b159c6b295f4d23d8
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2022-04-25 22:32:46 -0300

    feat(trabalho1): `socket_producer` to receive integer count as argument

--- trabalho1/src/bin/socket/producer.rs
+++ trabalho1/src/bin/socket/producer.rs
@@ -1,8 +1,17 @@
 use sd::producer::Producer;
+use std::env;
+use std::str::FromStr;
 
 fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        println!("Please specify how many numbers to generate.");
+        std::process::exit(1);
+    }
+    let number_count =
+        i32::from_str(&args[1]).expect("failed to parse how many numbers should be produced.");
+
     let mut producer =
         Producer::from_socket("127.0.0.1:31337".to_string()).expect("failed to create producer");
-    // producer.write(3);
-    producer.produce_random_ints(3).unwrap();
+    producer.produce_random_ints(number_count).unwrap();
 }
